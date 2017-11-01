cc7e34ae0e94525869bb07a5c16b54f8ab976966
Fabian Monge <fabian1mg@gmail.com>
2017-10-31 19:14:24 -0600

    Ejemoplo del proyecto

--- graficador.py
+++ graficador.py
@@ -2,15 +2,20 @@ import matplotlib.pyplot as plt
 import math
 
 # variables hardcodeadas 
-punto = [40,20,50] # x, y, z
-size = max(punto) * 1.1 # un 10% de olgura 
+punto = [4,2,5] # x, y, z
+size = max(punto) * 1.1 # tamaño grafico + 10% de olgura  
+
+# Base
+u = [1, 0, 1]
+v = [1, 2, 1]
+w = [0, 1, 2]
+
 
 def iso(punto):
-    x = round(punto[1] - punto[0],2)
-    y =  round( -0.5*(punto[0] + punto[1] - ( 2 * punto[2])) ,2)
+    x = round(punto[1] - punto[0], 2)
+    y =  round( -0.5 * (punto[0] + punto[1] - ( 2 * punto[2])), 2)
     return x,y
     
-
 def point2d(punto):
     x, y = iso(punto)
     plt.plot(x,y, 'ro') 
@@ -35,7 +40,7 @@ def pos(punto, pos, text):
         dashpush=dp,
         )
 
-def arrow2d(punto, fcolor, ecolor):
+def flecha(punto, fcolor, ecolor):
     x, y = iso(punto)
     ax.arrow(0,0, x, y, 
             head_width=0.5,
@@ -44,8 +49,26 @@ def arrow2d(punto, fcolor, ecolor):
             ec=ecolor
             ) 
 
+def arrow2d(puntoA ,puntoB, fcolor, ecolor):
+    x1, y1 = iso(puntoA)
+    x2, y2 = iso(puntoB)
+    ax.arrow(x1,y1, x2, y2, 
+            head_width=0.5,
+            head_length=0.5, 
+            fc=fcolor,
+            ec=ecolor
+            ) 
 
-#size_aux  = round(math.sqrt((size**2)*2),2)
+
+def porEscalar(punto, e):
+    result = []
+    for x in punto:
+        result.append(x * e)
+    return result
+
+
+# plano cartesiano 3d
+#
 eje_x = [size, 0 ,0]
 eje_y = [0, size, 0]
 eje_z = [0, 0, size]
@@ -53,20 +76,32 @@ eje_z = [0, 0, size]
 ax = plt.axes()
 
 # Aristas y vertices
-arrow2d(eje_y,'r','gray')
-pos(eje_y,1,"y")
-arrow2d(eje_x,'g','gray')
-pos(eje_x,1,"x")
-arrow2d(eje_z,'b','gray')
+flecha(eje_y,'r','gray')
+pos(eje_y,2,"y")
+flecha(eje_x,'g','gray')
+pos(eje_x,3,"x")
+flecha(eje_z,'b','gray')
 pos(eje_z,1,"z")
+#
+# plano cartesiano 3d
+
+arrow2d([0,0,0], porEscalar(u,4),'g','g')
+arrow2d([0,0,0], porEscalar(v,3),'g','g')
+arrow2d([0,0,0], porEscalar(w,3),'g','g')
+
+arrow2d([0,0,0], u,'b','b')
+arrow2d([0,0,0], v,'b','b')
+arrow2d([0,0,0], w,'b','b')
 
 
 # punto
-#punto_1 = [3,3,3] # x, y, z
 point2d(punto)
+pos(punto,3,'p')
 
-ax.grid() # fondo cuadriculado
-x,y = iso(punto)
+
+#x,y = iso(punto)
 #plt.axis([y,y,y,y])
+#ax.grid() # fondo cuadriculado
+
 plt.axis([-eje_y[1]*1.1, eje_y[1] * 1.1, -size * 1.1, eje_y[1] * 1.1])
 plt.show()
